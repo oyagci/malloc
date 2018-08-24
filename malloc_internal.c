5a669eebea9ebe8c231f6347204a60dd0ca1c1fa
Oguzhan YAGCI <oyagci@student.42.fr>
2018-08-24 14:34:42 +0200

    implement find_free_block

--- malloc_internal.c
+++ malloc_internal.c
@@ -6,7 +6,7 @@
 /*   By: oyagci <oyagci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/21 14:47:24 by oyagci            #+#    #+#             */
-/*   Updated: 2018/08/24 14:32:39 by oyagci           ###   ########.fr       */
+/*   Updated: 2018/08/24 14:34:18 by oyagci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,16 +64,34 @@ t_page		*init_new_page(t_page_type type, size_t size)
 	return (p);
 }
 
-t_block		*find_free_block(size_t size)
+t_block		*find_free_block(t_page_info *pinfo, size_t size)
 {
-	t_page_info		*info;
+	t_block	*b;
+	t_block	*c;
 
-	info = &g_pools[0];
-	if (!info->start)
+	if (!pinfo->start)
 	{
-		info->start = init_new_page();
+		if ((pinfo->start = init_new_page(TINY, M)) == NULL)
+			return (0);
 	}
-	(void)size;
+	b = pinfo->start->free;
+	if (b->size >= size && b->size > sizeof(t_block) + 16)
+	{
+		c = (t_block *)((t_byte *)(b + 1) + size);
+		c->size = b->size - size - sizeof(t_block) * 2;
+		if (b == pinfo->start->free)
+			pinfo->start->free = c;
+		else
+		{
+			printf("b->prev = %p\n", b->prev);
+			b->prev->next = c;
+			c->prev = b->prev;
+		}
+		b->size = size;
+		b->prev = NULL;
+		b->next = NULL;
+		return (b);
+	}
 	return (0);
 }
 
@@ -81,6 +99,6 @@ void		*malloc_internal(size_t size)
 {
 	t_block	*b;
 
-	b = find_free_block(size);
+	b = find_free_block(&g_pools[0], size);
 	return (b ? b + 1 : 0);
 }
