09fe5b1f2cd1014a199f3c87983ced1a9652190d
Oguzhan YAGCI <oyagci@student.42.fr>
2018-08-27 17:43:29 +0200

    add tests for add_block_to_free_list

--- test/free.c
+++ test/free.c
@@ -49,3 +49,50 @@ Test(add_block_to_free_list, basic2)
 	cr_assert(p[5] == pinfo.start->free);
 	cr_assert(pinfo.start->free->next == p[6]);
 }
+
+Test(add_block_to_free_list, basic3)
+{
+	t_page_info	pinfo;
+	void		*p[10];
+
+	pinfo.start = init_new_page(TINY, M);
+	cr_assert(pinfo.start);
+
+	for (int i = 0; i < 10; i++)
+	{
+		p[i] = find_free_block(&pinfo, 16);
+		cr_assert(p[i]);
+	}
+	add_block_to_free_list(p[5], &pinfo);
+	cr_assert(p[5] == pinfo.start->free);
+
+	add_block_to_free_list(p[6], &pinfo);
+	cr_assert(p[5] == pinfo.start->free);
+	cr_assert(pinfo.start->free->next == p[6]);
+
+	add_block_to_free_list(p[7], &pinfo);
+	cr_assert(p[5] == pinfo.start->free);
+	cr_assert(pinfo.start->free->next == p[6]);
+	cr_assert(pinfo.start->free->next->next == p[7]);
+}
+
+Test(add_block_to_free_list, basic_reverse)
+{
+	t_page_info	pinfo;
+	void		*p[10];
+
+	pinfo.start = init_new_page(TINY, M);
+	cr_assert(pinfo.start);
+
+	for (int i = 0; i < 10; i++)
+	{
+		p[i] = find_free_block(&pinfo, 16);
+		cr_assert(p[i]);
+	}
+	add_block_to_free_list(p[6], &pinfo);
+	cr_assert(p[6] == pinfo.start->free);
+
+	add_block_to_free_list(p[5], &pinfo);
+	cr_assert(p[5] == pinfo.start->free);
+	cr_assert(pinfo.start->free->next == p[6]);
+}
