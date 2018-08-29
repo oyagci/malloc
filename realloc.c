63f98133789775aca3ff1e8ee1f9e98373da902c
Oguzhan YAGCI <oyagci@student.42.fr>
2018-08-29 16:48:28 +0200

    add placeholder realloc (for testing)

--- realloc.c
+++ realloc.c
@@ -1,8 +1,26 @@
 #include "malloc.h"
+#include "libft/includes/libft.h"
+
+// void	*realloc(void *ptr, size_t size)
+// {
+// 	return (0);
+// }
 
 void	*realloc(void *ptr, size_t size)
 {
-	(void)ptr;
-	(void)size;
-	return (0);
+	if (!ptr)
+		return (malloc(size));
+
+	t_block	*old = (t_block *)ptr - 1;
+
+	if (old->size >= size)
+		return (ptr);
+
+	void	*newp = malloc(size);
+
+	if (newp == 0)
+		return (0);
+	ft_bzero(newp, size);
+	ft_memcpy(newp, ptr, old->size);
+	return (newp);
 }
