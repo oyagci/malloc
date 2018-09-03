b1777671d8e1136148bcc0d19eedef2fc0502bb1
Oguzhan YAGCI <oyagci@student.42.fr>
2018-09-03 15:53:56 +0200

    add page to pools if pool is full

--- malloc_internal.c
+++ malloc_internal.c
@@ -6,7 +6,7 @@
 /*   By: oyagci <oyagci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/21 14:47:24 by oyagci            #+#    #+#             */
-/*   Updated: 2018/09/03 12:48:13 by oyagci           ###   ########.fr       */
+/*   Updated: 2018/09/03 15:52:56 by oyagci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,15 +44,24 @@ void		*map_page(size_t size)
 
 t_block		*find_free_block(t_page_info *pinfo, size_t size)
 {
+	t_page	*p;
 	t_block	*b;
 	t_block	*rmder;
 
+	/* TODO: Initialisation should not happen here */
 	if (!pinfo->start)
 		if (NULL == (pinfo->start = init_new_page(TINY, M)))
 			return (0);
-	b = pinfo->start->free;
-	while (b && b->size < size)
-		b = b->next;
+	p = pinfo->start;
+	while (p)
+	{
+		b = p->free;
+		while (b && b->size < size)
+			b = b->next;
+		if (b && b->size >= size)
+			break ;
+		p = p->next;
+	}
 	if (!b)
 		return (0);
 	if (b->size - size > sizeof(t_block) + 16)
@@ -60,14 +69,24 @@ t_block		*find_free_block(t_page_info *pinfo, size_t size)
 		rmder = (t_block *)((t_byte *)(b + 1) + size);
 		rmder->size = b->size - size - sizeof(t_block) * 2;
 		b->size = size;
-		add_remainder_to_free_list(&pinfo->start->free, b, rmder);
+		add_remainder_to_free_list(&p->free, b, rmder);
 	}
 	else
-		remove_block(&pinfo->start->free, b);
+		remove_block(&p->free, b);
 	b->prev = NULL;
 	b->next = NULL;
 	b->is_free = 0;
 	return (b);
+}
+
+void		append_page_to_pool(t_page_info *pool)
+{
+	t_page	*p;
+
+	p = pool->start;
+	while (p->next)
+		p = p->next;
+	p->next = init_new_page(pool->type, pool->start->size);
 }
 
 void		*malloc_internal(size_t size, t_page_info *pools)
@@ -78,5 +97,10 @@ void		*malloc_internal(size_t size, t_page_info *pools)
 	if (size > TINY && size < SMALL)
 		size = round_up(size, SMALL_RES);
 	b = find_free_block(pools, size);
+	if (!b)
+	{
+		append_page_to_pool(pools);
+		b = find_free_block(pools, size);
+	}
 	return (b ? b + 1 : 0);
 }
