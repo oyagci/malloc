8806b0f7fa03f9a3a02aeaaab68d306c299070e3
Oguzhan YAGCI <oyagci@student.42.fr>
2018-09-06 15:46:48 +0200

    fix calloc not setting memory to 0 when size == 0 or n == 0

--- calloc.c
+++ calloc.c
@@ -6,18 +6,22 @@
 /*   By: oyagci <student@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 12:55:38 by oyagci            #+#    #+#             */
-/*   Updated: 2018/09/03 16:02:24 by oyagci           ###   ########.fr       */
+/*   Updated: 2018/09/06 13:24:37 by oyagci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 #include "libft/includes/libft.h"
+#include <stdlib.h>
 
 void	*calloc(size_t size, size_t n)
 {
 	void	*mem;
 
 	mem = malloc(size * n);
-	ft_bzero(mem, size * n);
+	if (size == 0)
+		ft_bzero(mem, 16);
+	else
+		ft_bzero(mem, size * n);
 	return (mem);
 }
