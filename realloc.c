85c5305ae3083f2c0e145abccbd73496177655d9
Oguzhan YAGCI <oyagci@student.42.fr>
2018-09-06 15:47:22 +0200

    add reallocf

--- realloc.c
+++ realloc.c
@@ -6,17 +6,12 @@
 /*   By: oyagci <oyagci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 13:23:18 by oyagci            #+#    #+#             */
-/*   Updated: 2018/09/03 13:23:49 by oyagci           ###   ########.fr       */
+/*   Updated: 2018/09/06 13:42:58 by oyagci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 #include "libft/includes/libft.h"
-
-// void	*realloc(void *ptr, size_t size)
-// {
-// 	return (0);
-// }
 
 void	*realloc(void *ptr, size_t size)
 {
@@ -35,4 +30,9 @@ void	*realloc(void *ptr, size_t size)
 	ft_bzero(newp, size);
 	ft_memcpy(newp, ptr, old->size);
 	return (newp);
+}
+
+void	*reallocf(void *ptr, size_t size)
+{
+	return (realloc(ptr, size));
 }
