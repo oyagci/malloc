768ad27fec44b75be0151ea066e92ca0587e13b2
Oguzhan YAGCI <oyagci@student.42.fr>
2018-09-10 13:42:06 +0200

    add 42_Correction tests

---
+++ test/42_Corrections/test.sh
@@ -0,0 +1,24 @@
+gcc test0.c -o bin/test0
+gcc test1.c -o bin/test1
+gcc test2.c -o bin/test2
+gcc test3.c -o bin/test3
+gcc test3bis.c -o bin/test3bis
+gcc test4.c -o bin/test4
+
+echo -- test0:
+./run.sh /usr/bin/time -l bin/test0
+read
+echo -- test1:
+./run.sh /usr/bin/time -l bin/test1
+read
+echo -- test2:
+./run.sh /usr/bin/time -l bin/test2
+read
+echo -- test3:
+./run.sh /usr/bin/time -l bin/test3
+read
+echo -- test3bis:
+./run.sh /usr/bin/time -l bin/test3bis
+read
+echo -- test4:
+./run.sh /usr/bin/time -l bin/test4
---
+++ test/42_Corrections/test0.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int main()
+{
+	void *addr;
+
+	int i = 0;
+	while (i < 1024)
+	{
+		i += 1;
+	}
+	return (0);
+}
