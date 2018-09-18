8c14b9d10b87acc0c3fc80ac0ae146cc53c06af0
Oguzhan YAGCI <oyagci@student.42.fr>
2018-09-18 10:52:59 +0200

    remove debugging expressions

--- merge_free_blocks.c
+++ merge_free_blocks.c
@@ -6,45 +6,26 @@
 /*   By: oyagci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/13 12:38:51 by oyagci            #+#    #+#             */
-/*   Updated: 2018/09/17 10:36:37 by oyagci           ###   ########.fr       */
+/*   Updated: 2018/09/18 09:45:05 by oyagci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
-#include <assert.h>
 
 void	try_merge(t_block *b)
 {
-	int count = 0;
 	t_block	*next;
 
-//	ft_putendl_fd("--- try_merge", 2);
-//	ft_putnbr_fd(b->size + sizeof(t_block), 2);
-//	ft_putchar_fd('\n',2);
-	while (42)
+	next = (t_block *)((t_byte *)(b + 1) + b->size);
+	if (b->is_free && next->is_free)
 	{
-		count++;
-		next = (t_block *)((t_byte *)(b + 1) + b->size);
-		if (b->is_free && next->is_free)
-		{
-//			ft_putstr_fd("merging ", 2);
-//			ft_putnbr_fd(next->size + sizeof(t_block), 2);
-//			ft_putchar_fd('\n', 2);
-			assert(next->prev == b);
-			assert(b->next == next);
-			b->size = b->size + next->size + sizeof(t_block);
-			b->next = next->next;
-			if (b->next)
-				b->next->prev = b;
-			next->prev = (void*)0xDEADBEEF;
-			next->next = (void*)0xDEADBEEF;
-		}
-		else
-			break ;
+		b->size = b->size + next->size + sizeof(t_block);
+		b->next = next->next;
+		b->next->prev = b;
+		next->prev = (void*)0xDEADBEEF;
+		next->next = (void*)0xDEADBEEF;
+		try_merge(b);
 	}
-//	ft_putnbr_fd(b->size + sizeof(t_block), 2);
-//	ft_putchar_fd('\n',2);
-//	ft_putchar_fd('\n',2);
 }
 
 void	merge_pools_free_blocks(t_page_info *pool)
@@ -76,7 +57,6 @@ void	merge_free_blocks(t_page_info *pools)
 		return ;
 	else
 		count = 10;
-//	ft_putendl_fd("merge_free_blocks", 2);
 	i = 0;
 	while (i < 3)
 	{
