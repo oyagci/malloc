2adc645866621edf3c0f4661f9466305f59497e9
Oguzhan YAGCI <oyagci@student.42.fr>
2018-09-18 14:11:53 +0200

    fix rmder->prev not being initialized

--- malloc_internal_util.c
+++ malloc_internal_util.c
@@ -6,7 +6,7 @@
 /*   By: oyagci <oyagci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 12:46:18 by oyagci            #+#    #+#             */
-/*   Updated: 2018/09/11 15:11:26 by oyagci           ###   ########.fr       */
+/*   Updated: 2018/09/18 13:51:08 by oyagci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,21 @@ void		remove_block(t_block **free_list, t_block *to_remove)
 
 void		add_remainder_to_free_list(
 		t_block **free_list,
-		t_block *b,
+		t_block *big,
 		t_block *rmder
 )
 {
-	if (b == *free_list)
+	if (big == *free_list)
+	{
+		rmder->prev = 0;
 		*free_list = rmder;
+	}
 	else
 	{
-		rmder->prev = b->prev;
+		rmder->prev = big->prev;
 		rmder->prev->next = rmder;
 	}
-	rmder->next = b->next;
+	rmder->next = big->next;
 	if (rmder->next)
 		rmder->next->prev = rmder;
 }
